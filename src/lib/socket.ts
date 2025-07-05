fad79920b16e2534f8e2210048c6459e47375118
PandeyAnukrati <anukrati240@gmail.com>
2025-07-06 00:50:27 +0530

    last

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,7 +4,8 @@ let socket: Socket | null = null;
 
 export function getSocket() {
   if (!socket) {
-    socket = io("http://localhost:5000"); // Backend URL
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000";
+    socket = io(backendUrl);
   }
   return socket;
 }
